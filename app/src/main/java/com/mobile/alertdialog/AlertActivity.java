575a15494693caf89f2b3620a5e6f73a90e125a3
MonkeyBob <1018472264@qq.com>
2016-05-07 16:13:44 +0800

    dialog对话框

---
+++ app/src/main/java/com/mobile/alertdialog/AlertActivity.java
@@ -0,0 +1,41 @@
+package com.mobile.alertdialog;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AlertActivity extends AppCompatActivity {
+
+    private Button mButton1;
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+
+        mButton1 = (Button) findViewById(R.id.myButton1);
+        mButton1.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                new AlertDialog.Builder(AlertActivity.this)
+                        .setTitle(R.string.app_about)
+                        .setMessage(R.string.app_about_msg)
+                        .setPositiveButton(R.string.str_ok, new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setNegativeButton(R.string.str_cancle, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).show();
+            }
+        });
+    }
+}
